feae574b5c03eece97e6f6c2e7d46e19b6a58e0b
lemmusm <lemmusm@gmail.com>
2019-10-28 21:52:46 -0600

    Use props

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
 import './styles/Card.css';
 import exerciseImg from '../assets/images/exercise.png';
+import circlesImg from '../assets/images/circles.png';
 
 export default class Card extends Component {
   render() {
     return (
-      <div className="card mx-auto Fitness-Card">
+      <div
+        className="card mx-auto Fitness-Card"
+        style={{
+          backgroundImage: `url(${circlesImg}), linear-gradient(to right, ${this.props.leftColor}, ${this.props.rightColor})`
+        }}
+      >
         <div className="card-body">
           <div className="row center">
             <div className="col-6">
-              <img src={exerciseImg} className="float-right" />
+              <img src={this.props.img} className="float-right" />
             </div>
             <div className="col-6 Fitness-Card-Info">
-              <h1>Technique Guides</h1>
-              <p>Learn amazing street workout and calisthenics</p>
+              <h1>{this.props.title}</h1>
+              <p>{this.props.description}</p>
             </div>
           </div>
         </div>
--- src/index.js
+++ src/index.js
@@ -5,7 +5,16 @@ import './index.css';
 import Card from './components/Card';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Card />, document.getElementById('root'));
+ReactDOM.render(
+  <Card
+    title="Technique Guides"
+    description="Learn amazing street workout and calisthenics"
+    img="https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06"
+    leftColor="#a74cf2"
+    rightColor="#617bfb"
+  />,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
