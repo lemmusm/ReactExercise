a462e1071db19b1fde9041fbe04b21f52c04a0b4
lemmusm <lemmusm@gmail.com>
2019-10-28 22:02:03 -0600

    Updated how to use props

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,21 +5,22 @@ import circlesImg from '../assets/images/circles.png';
 
 export default class Card extends Component {
   render() {
+    const { title, description, img, leftColor, rightColor } = this.props;
     return (
       <div
         className="card mx-auto Fitness-Card"
         style={{
-          backgroundImage: `url(${circlesImg}), linear-gradient(to right, ${this.props.leftColor}, ${this.props.rightColor})`
+          backgroundImage: `url(${circlesImg}), linear-gradient(to right, ${leftColor}, ${rightColor})`
         }}
       >
         <div className="card-body">
           <div className="row center">
             <div className="col-6">
-              <img src={this.props.img} className="float-right" />
+              <img src={img} className="float-right" />
             </div>
             <div className="col-6 Fitness-Card-Info">
-              <h1>{this.props.title}</h1>
-              <p>{this.props.description}</p>
+              <h1>{title}</h1>
+              <p>{description}</p>
             </div>
           </div>
         </div>
