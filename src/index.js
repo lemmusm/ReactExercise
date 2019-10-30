445f4dd0549c819a9b3fcb7bff05c2b796a4e8ec
lemmusm <lemmusm@gmail.com>
2019-10-30 10:33:31 -0600

    Create Welcome component
    	.- Functional component

--- src/index.js
+++ src/index.js
@@ -3,16 +3,20 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import Card from './components/Card';
+import Welcome from './components/Welcome';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <Card
-    title="Technique Guides"
-    description="Learn amazing street workout and calisthenics"
-    img="https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06"
-    leftColor="#a74cf2"
-    rightColor="#617bfb"
-  />,
+  <div>
+    <Welcome username="Manuel" />
+    <Card
+      title="Technique Guides"
+      description="Learn amazing street workout and calisthenics"
+      img="https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06"
+      leftColor="#a74cf2"
+      rightColor="#617bfb"
+    />
+  </div>,
   document.getElementById('root')
 );
 
