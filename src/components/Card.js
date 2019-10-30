7dab3ebd681ba77061e442a6f097ce00577a83cf
lemmusm <lemmusm@gmail.com>
2019-10-30 14:17:48 -0600

    Create ExerciseList component
    	.- Iterate data from object

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,26 +1,8 @@
 import React, { Component } from 'react';
 import './styles/Card.css';
-import exerciseImg from '../assets/images/exercise.png';
 import circlesImg from '../assets/images/circles.png';
 
 export default class Card extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      image:
-        'https://www.stickpng.com/assets/images/580b57fcd9996e24bc43c52f.png'
-    };
-  }
-
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        image:
-          'https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06'
-      });
-    }, 5000);
-  }
-
   render() {
     const { title, description, img, leftColor, rightColor } = this.props;
     return (
@@ -33,7 +15,7 @@ export default class Card extends Component {
         <div className="card-body">
           <div className="row center">
             <div className="col-6">
-              <img src={this.state.image} className="float-right" />
+              <img src={img} className="float-right" />
             </div>
             <div className="col-6 Fitness-Card-Info">
               <h1>{title}</h1>
